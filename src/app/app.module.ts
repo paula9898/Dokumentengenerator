0988e368e849f9b266c02ae38a6a478a2daed3f2
paula9898 <otrebskapaulina@gmail.com>
2023-10-17 16:02:55 +0200

    add Material Module, style menu component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,24 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MaterialModule } from 'src/Material.Module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, MenuComponent],
-  imports: [BrowserModule, AppRoutingModule, MatToolbarModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
